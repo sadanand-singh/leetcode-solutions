48f2c96bb909f256098f01fe4bc0ec42bf9a3c87
Sadanand Singh <sn.singh@samsung.com>
2018-07-17 17:16:44 -0700

    add additional new submissions

---
+++ submissions/139.word-break.164350347.ac.python3.py
@@ -0,0 +1,117 @@
+#
+# [139] Word Break
+#
+# https://leetcode.com/problems/word-break/description/
+#
+# algorithms
+# Medium (32.05%)
+# Total Accepted:    227.1K
+# Total Submissions: 708.7K
+# Testcase Example:  '"leetcode"\n["leet","code"]'
+#
+# Given a non-empty string s and a dictionary wordDict containing a list of
+# non-empty words, determine if s can be segmented into a space-separated
+# sequence of one or more dictionary words.
+# 
+# Note:
+# 
+# 
+# The same word in the dictionary may be reused multiple times in the
+# segmentation.
+# You may assume the dictionary does not contain duplicate words.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: s = "leetcode", wordDict = ["leet", "code"]
+# Output: true
+# Explanation: Return true because "leetcode" can be segmented as "leet
+# code".
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: s = "applepenapple", wordDict = ["apple", "pen"]
+# Output: true
+# Explanation: Return true because "applepenapple" can be segmented as "apple
+# pen apple".
+# Note that you are allowed to reuse a dictionary word.
+# 
+# 
+# Example 3:
+# 
+# 
+# Input: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+# Output: false
+# 
+# 
+#
+#
+# [139] Word Break
+#
+# https://leetcode.com/problems/word-break/description/
+#
+# algorithms
+# Medium (32.05%)
+# Total Accepted:    227.1K
+# Total Submissions: 708.7K
+# Testcase Example:  '"leetcode"\n["leet","code"]'
+#
+# Given a non-empty string s and a dictionary wordDict containing a list of
+# non-empty words, determine if s can be segmented into a space-separated
+# sequence of one or more dictionary words.
+#
+# Note:
+#
+#
+# The same word in the dictionary may be reused multiple times in the
+# segmentation.
+# You may assume the dictionary does not contain duplicate words.
+#
+#
+# Example 1:
+#
+#
+# Input: s = "leetcode", wordDict = ["leet", "code"]
+# Output: true
+# Explanation: Return true because "leetcode" can be segmented as "leet
+# code".
+#
+#
+# Example 2:
+#
+#
+# Input: s = "applepenapple", wordDict = ["apple", "pen"]
+# Output: true
+# Explanation: Return true because "applepenapple" can be segmented as "apple
+# pen apple".
+# Note that you are allowed to reuse a dictionary word.
+#
+#
+# Example 3:
+#
+#
+# Input: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+# Output: false
+#
+#
+#
+class Solution:
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: bool
+        """
+
+        pos = [-1] * (1 + len(s))
+        pos[0] = 0
+        for i in range(len(s)):
+            if pos[i] != -1:
+                for j in range(i + 1, len(s) + 1):
+                    if s[i:j] in wordDict:
+                        pos[j] = i
+        return pos[len(s)] != -1
+
